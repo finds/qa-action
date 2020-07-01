de3ee7989321a9843eea04e846a0a2609b37684e
Matt Mayer <matt@matthewmayer.co.uk>
2020-07-01 14:40:01 +0700

    em logs

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const label = core.getInput('label');
   const myToken = core.getInput('token');
   const octokit = github.getOctokit(myToken)
 
   const time = (new Date()).toTimeString();
   core.setOutput("message", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
   const payload = github.context.payload
-  console.log(JSON.stringify(github.context.payload, undefined, 2))
   if (payload.action=="closed") {
     let [owner, repo] = payload.repository.full_name.split("/")
     octokit.issues.addLabels({
