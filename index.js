92ba4cdba697335e95e46ef5fd6af8d3e5ab8494
Matt Mayer <matt@matthewmayer.co.uk>
2020-07-01 14:35:40 +0700

    fix octokit

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const label = core.getInput('label');
+  const myToken = core.getInput('token');
+  const octokit = github.getOctokit(myToken)
+
   const time = (new Date()).toTimeString();
   core.setOutput("message", time);
   // Get the JSON webhook payload for the event that triggered the workflow
@@ -11,7 +14,7 @@ try {
   console.log(JSON.stringify(github.context.payload, undefined, 2))
   if (payload.action=="closed") {
     let [owner, repo] = payload.repository.full_name.split("/")
-    github.issues.addLabels({
+    octokit.issues.addLabels({
         owner,
         repo,
         issue_number:payload.issue.number,
